d8117629e40d3667296deec40c8f4ce2890a4d2f
Thomas Woltering <wart3k@gmx.de>
2023-01-27 21:50:04 +0100

    added whitespace unit tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -36,6 +36,35 @@ TEST_CASE( "Testing convert float to hex example values", "[float-to-hex-example
     REQUIRE(testConverter.convertFloatToHex("1.5") == converter_pair);
 }
 
+TEST_CASE( "Testing convert float to hex with whitespaces in float values", "[float-to-hex-whitespaces]" ) {
+    ConverterData testConverter;
+
+    auto converter_pair = std::pair<ConverterStatus, std::string>{};
+
+    /* Test cases with Status = OK and values from example */
+    converter_pair.first = ConverterStatus::OK;
+    converter_pair.second = "0xbfc00000";
+    REQUIRE(testConverter.convertFloatToHex("- 1. 5") == converter_pair);
+
+    converter_pair.second = "0xbf800000";
+    REQUIRE(testConverter.convertFloatToHex("    -1.0    ") == converter_pair);
+
+    converter_pair.second = "0xbf000000";
+    REQUIRE(testConverter.convertFloatToHex(" - 0 . 5 ") == converter_pair);
+
+    converter_pair.second = "0x00000000";
+    REQUIRE(testConverter.convertFloatToHex("0.0       ") == converter_pair);
+
+    converter_pair.second = "0x3f000000";
+    REQUIRE(testConverter.convertFloatToHex("0 .     5") == converter_pair);
+
+    converter_pair.second = "0x3f800000";
+    REQUIRE(testConverter.convertFloatToHex(" 1 .      0 ") == converter_pair);
+
+    converter_pair.second = "0x3fc00000";
+    REQUIRE(testConverter.convertFloatToHex("          1       .            5             ") == converter_pair);
+}
+
 TEST_CASE( "Testing convert hex to float with example values", "[hex-to-float-example-val]" ) {
     ConverterData testConverter;
 
@@ -64,6 +93,30 @@ TEST_CASE( "Testing convert hex to float with example values", "[hex-to-float-ex
     REQUIRE(testConverter.convertHexToFloat("0x3fc00000") == converterPair);
 }
 
-TEST_CASE("Testing checkHexReadString", "[checkHexReadString]") {
-    auto readMock = std::pair<ConverterStatus, std::string>{};
+TEST_CASE( "Testing convert hex to float values with whitespaces in hex values", "[float-to-hex-whitespaces]" ) {
+    ConverterData testConverter;
+
+    auto converterPair = std::pair<ConverterStatus, std::string>{};
+    /* Test cases with Status = OK and values from example */
+    converterPair.first = ConverterStatus::OK;
+    converterPair.second = "-1.500000";
+    REQUIRE(testConverter.convertHexToFloat(" 0x bf c000 00") == converterPair);
+
+    converterPair.second = "-1.000000";
+    REQUIRE(testConverter.convertHexToFloat("0xbf  8000  00  ") == converterPair);
+
+    converterPair.second = "-0.500000";
+    REQUIRE(testConverter.convertHexToFloat("  0 x bf 000 0 00") == converterPair);
+
+    converterPair.second = "0.000000";
+    REQUIRE(testConverter.convertHexToFloat(" 0 x 0 0 0 0 0 0  0 0") == converterPair);
+
+    converterPair.second = "0.500000";
+    REQUIRE(testConverter.convertHexToFloat("               0x3f000000                ") == converterPair);
+
+    converterPair.second = "1.000000";
+    REQUIRE(testConverter.convertHexToFloat("0x 3f8    0  0 00  0") == converterPair);
+
+    converterPair.second = "1.500000";
+    REQUIRE(testConverter.convertHexToFloat("0 x 3fc00000") == converterPair);
 }
